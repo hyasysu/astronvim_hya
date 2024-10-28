f80bb310ea609d17fe4b2196c077d9345ee0c8e2
hyasysu <hyasysujx@163.com>
2024-10-28 09:11:39 +0800

    switch colorscheme

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -10,8 +10,8 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
-    -- colorscheme = "catppuccin",
-    colorscheme = "solarized-osaka",
+    colorscheme = "catppuccin",
+    -- colorscheme = "solarized-osaka",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
