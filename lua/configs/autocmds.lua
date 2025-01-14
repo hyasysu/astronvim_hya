08e1c63d45a0f937262d23d90bc9d158767775fe
hyasysu <hyasysujx@163.com>
2025-01-14 14:27:29 +0800

    set winfixbuf

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -21,3 +21,28 @@ vim.api.nvim_create_autocmd("TermOpen", {
     end
   end,
 })
+
+-- set winfixbuf to especial windows
+vim.api.nvim_create_augroup("IrreplaceableWindows", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = "IrreplaceableWindows",
+  pattern = "*",
+  callback = function()
+    local filetypes = { "OverseerList", "neo-tree", "toggleterm" }
+    local buftypes = { "nofile", "terminal" }
+    if vim.tbl_contains(buftypes, vim.bo.buftype) and vim.tbl_contains(filetypes, vim.bo.filetype) then
+      vim.cmd "set winfixbuf"
+    end
+  end,
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = "IrreplaceableWindows",
+  pattern = "*",
+  callback = function()
+    local filetypes = { "OverseerList", "neo-tree" }
+    local buftypes = { "nofile" }
+    if vim.tbl_contains(buftypes, vim.bo.buftype) and vim.tbl_contains(filetypes, vim.bo.filetype) then
+      vim.cmd "set winfixbuf"
+    end
+  end,
+})
