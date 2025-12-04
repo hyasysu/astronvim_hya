ed0d5bc95a40ef2de8b20c1611f504668d9fd266
hyasysu <hyasysujx@163.com>
2025-12-04 14:29:54 +0800

    only set wiinfixbuf on terminal
    Refactor autocmds to limit filetypes for TermEnter and remove BufWinEnter autocmd.

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -28,7 +28,8 @@ vim.api.nvim_create_autocmd("TermEnter", {
   group = "IrreplaceableWindows",
   pattern = "*",
   callback = function()
-    local filetypes = { "OverseerList", "neo-tree", "toggleterm" }
+    -- local filetypes = { "OverseerList", "neo-tree", "toggleterm" }
+    local filetypes = { "toggleterm" }
     local buftypes = { "nofile", "terminal" }
     if vim.tbl_contains(buftypes, vim.bo.buftype) and vim.tbl_contains(filetypes, vim.bo.filetype) then
       vim.notify("setwinfixbuf", vim.log.levels.INFO)
@@ -36,14 +37,14 @@ vim.api.nvim_create_autocmd("TermEnter", {
     end
   end,
 })
-vim.api.nvim_create_autocmd("BufWinEnter", {
-  group = "IrreplaceableWindows",
-  pattern = "*",
-  callback = function()
-    local filetypes = { "OverseerList", "neo-tree" }
-    local buftypes = { "nofile" }
-    if vim.tbl_contains(buftypes, vim.bo.buftype) and vim.tbl_contains(filetypes, vim.bo.filetype) then
-      vim.cmd "set winfixbuf"
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWinEnter", {
+--   group = "IrreplaceableWindows",
+--   pattern = "*",
+--   callback = function()
+--     local filetypes = { "OverseerList", "neo-tree" }
+--     local buftypes = { "nofile" }
+--     if vim.tbl_contains(buftypes, vim.bo.buftype) and vim.tbl_contains(filetypes, vim.bo.filetype) then
+--       vim.cmd "set winfixbuf"
+--     end
+--   end,
+-- })
